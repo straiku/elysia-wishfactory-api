9da376a16c5dcbedc08d399928720b9e35ea9c90
YarielZC <yarielzamoracueto@gmail.com>
2026-05-12 21:47:58 -0400

    refactor(index): add port enviroment variable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Elysia } from "elysia";
 import { authRoutes } from "./auth/auth.routes";
+import { config } from './config';
 const app = new Elysia()
 .use(authRoutes)
-.listen(3000);
+.listen(config.backend.port ?? 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
